781ac74236c26ac7632ec7e78e3ecbc1de9dcace
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-05-01 11:04:48 -0400

    Fixed all the compiler errors.

--- src/game.rs
+++ src/game.rs
@@ -4,14 +4,21 @@ use std::fmt::Display;
 use std::fmt::Error;
 
 use crate::game::Color::*;
+use crate::game::TurnPhase::*;
+use crate::game::TurnError::*;
 
 // Elementary types.
 
-#[derive(Clone, Copy)] struct Tile {x: usize, y: usize}
-#[derive(Clone, Copy)] enum Color {Black, White, Empty}
-#[derive(Clone, Copy)] enum TurnPhase {Play, Spin}
-#[derive(Clone, Copy)] enum GameOutcome {BlackWin, WhiteWin, Stalemate, DoubleWin}
-#[derive(Clone, Copy)] enum Error {
+#[derive(Clone, Copy)] pub struct Tile {x: usize, y: usize}
+#[derive(Clone, Copy, PartialEq, Eq)] pub enum Color {Black, White, Empty}
+#[derive(Clone, Copy, PartialEq, Eq)] pub enum TurnPhase {Play, Spin}
+#[derive(Clone, Copy, PartialEq, Eq)] pub enum GameOutcome {
+    BlackWin,
+    WhiteWin,
+    Stalemate,
+    DoubleWin,
+}
+#[derive(Clone, Copy, PartialEq, Eq)] pub enum TurnError {
     GameAlreadyOver,
     NotYourTurn,
     PlayDuringSpinPhase,
@@ -20,7 +27,7 @@ use crate::game::Color::*;
     StoneAlreadyThere,
 }
 
-type ActionResult = Result<Option<GameOutcome>, String>;
+pub type TurnResult = Result<Option<GameOutcome>, TurnError>;
 
 // Game type.
 
@@ -43,15 +50,15 @@ impl Twirl {
         }
     }
 
-    pub fn play(self, player: Color, play: Tile) -> ActionResult {
-        if self.outcome != None             {return Err(GameAlreadyOver);}
-        if self.whose_turn != player        {return Err(NotYourTurn);}
-        if self.turn_phase != Play          {return Err(PlayDuringSpinPhase);}
-        if play.x >= self.size              {return Err(InvalidLocation);}
-        if play.y >= self.size              {return Err(InvalidLocation);}
-        if board[play.y][play.x] != Empty   {return Err(StoneAlreadyThere);}
+    pub fn play(mut self, player: Color, play: Tile) -> TurnResult {
+        if self.outcome != None                 {return Err(GameAlreadyOver);}
+        if self.whose_turn != player            {return Err(NotYourTurn);}
+        if self.turn_phase != Play              {return Err(PlayDuringSpinPhase);}
+        if play.x >= self.size                  {return Err(InvalidLocation);}
+        if play.y >= self.size                  {return Err(InvalidLocation);}
+        if self.board[play.y][play.x] != Empty  {return Err(StoneAlreadyThere);}
 
-        board[play.y][play.x] = player;
+        self.board[play.y][play.x] = player;
 
         // TODO: If we end up treating the play phase as a distinct action, then
         // update the game's outcome here.
@@ -59,7 +66,7 @@ impl Twirl {
         Ok(self.outcome)
     }
 
-    pub fn spin(self, player: Color, ul: Tile, size: usize) -> ActionResult {
+    pub fn spin(mut self, player: Color, ul: Tile, size: usize) -> TurnResult {
         if self.outcome != None             {return Err(GameAlreadyOver);}
         if self.whose_turn != player        {return Err(NotYourTurn);}
         if self.turn_phase != Spin          {return Err(SpinDuringPlayPhase);}
