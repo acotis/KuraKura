0609184712e1c6425b271dc48fd403a8132f2411
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-05-01 10:55:42 -0400

    Implemented untested new paradigm.

--- src/game.rs
+++ src/game.rs
@@ -17,7 +17,6 @@ use crate::game::Color::*;
     PlayDuringSpinPhase,
     SpinDuringPlayPhase,
     InvalidLocation,
-    InvalidSpinTarget,
     StoneAlreadyThere,
 }
 
@@ -26,6 +25,7 @@ type ActionResult = Result<Option<GameOutcome>, String>;
 // Game type.
 
 pub struct Twirl {
+    size:       usize,
     board:      Vec<Vec<Color>>,
     outcome:    Option<GameOutcome>, // None until the game is over
     whose_turn: Color,
@@ -35,6 +35,7 @@ pub struct Twirl {
 impl Twirl {
     pub fn new(size: usize) -> Self {
         Twirl {
+            size:       size,
             board:      vec![vec![Empty; size]; size],
             outcome:    None,
             whose_turn: Black,
@@ -43,44 +44,32 @@ impl Twirl {
     }
 
     pub fn play(self, player: Color, play: Tile) -> ActionResult {
-        // Game cannot already be over.
-        // Must be your turn.
-        // Must be in the "play" phase of your turn.
-        // Play parameters must be valid:
-        //     Play tile is on the board.
-        // Current status of play tile must be empty.
-
         if self.outcome != None             {return Err(GameAlreadyOver);}
         if self.whose_turn != player        {return Err(NotYourTurn);}
         if self.turn_phase != Play          {return Err(PlayDuringSpinPhase);}
-        if !validate_tile(play)             {return Err(InvalidLocation);}
+        if play.x >= self.size              {return Err(InvalidLocation);}
+        if play.y >= self.size              {return Err(InvalidLocation);}
         if board[play.y][play.x] != Empty   {return Err(StoneAlreadyThere);}
 
+        board[play.y][play.x] = player;
 
+        // TODO: If we end up treating the play phase as a distinct action, then
+        // update the game's outcome here.
 
-        if !validate_tile(ul)               {return Fail;}
-        if !validate_tile(br)               {return Fail;}
-        if br.x < ul.x                      {return Fail;}
-        if br.y < ul.y                      {return Fail;}
-        if bx.x - ul.x != br.y - ul.y       {return Fail;}
+        Ok(self.outcome)
     }
 
-    pub fn spin(self, player: Color, ul: Tile, br: Tile) -> ActionResult
-        // Game cannot already be over.
-        // Must be your turn.
-        // Must be in the "spin" phase of your turn.
-        // Spin parameters must be valid:
-        //     UL tile is on the board.
-        //     BR tile is on the board.
-        //     BR tile is lower and righter than UL tile (or on top of it).
-        //     UL and BR together cut a square region of the board (not just rectangular).
+    pub fn spin(self, player: Color, ul: Tile, size: usize) -> ActionResult {
+        if self.outcome != None             {return Err(GameAlreadyOver);}
+        if self.whose_turn != player        {return Err(NotYourTurn);}
+        if self.turn_phase != Spin          {return Err(SpinDuringPlayPhase);}
+        if ul.x + size >= self.size         {return Err(InvalidLocation);}
+        if ul.y + size >= self.size         {return Err(InvalidLocation);}
 
+        // TODO: Perform the spin.
+        // TODO: Update the game's outcome.
 
-
-    fn validate_tile(self, tile: Tile) -> bool {
-        if tile.x >= self.board.len() {return false;}
-        if tile.y >= self.board.len() {return false;}
-        return true;
+        Ok(self.outcome)
     }
 }
 
