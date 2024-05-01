eac9987a2fc12cb1a48842a3a41ca289189db8ae
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-05-01 10:15:53 -0400

    Working on implementing basic game engine.

--- src/game.rs
+++ src/game.rs
@@ -5,6 +5,11 @@ use std::fmt::Error;
 
 use crate::game::Color::*;
 
+struct Tile {
+    x: usize,
+    y: usize,
+}
+
 #[derive(Clone, Copy)]
 enum Color {
     Black,
@@ -23,7 +28,6 @@ enum PlayResult {
     Succeed(Option<GameOutcome>),
 }
 
-
 pub struct Twirl {
     board:      Vec<Vec<Color>>,
     next_turn:  Color,
@@ -37,6 +41,34 @@ impl Twirl {
             next_turn:  Black,
             outcome:    None,
         }
+    }
+
+    pub fn play(self, player: Color, play: Tile, ul: Tile, br: Tile) -> PlayResult {
+        // Game cannot already be over.
+        // Must be your turn.
+        // Play parameters must be valid:
+        //     Play tile is on the board.
+        //     UL tile is on the board.
+        //     BR tile is on the board.
+        //     BR tile is lower and righter than UL tile (or on top of it).
+        //     UL and BR together cut a square region of the board (not just rectangular).
+        // Current status of play tile must be empty.
+
+        if self.outcome != None             {return Fail;}
+        if player != self.next_turn         {return Fail;}
+        if !validate_tile(play)             {return Fail;}
+        if !validate_tile(ul)               {return Fail;}
+        if !validate_tile(br)               {return Fail;}
+        if br.x < ul.x                      {return Fail;}
+        if br.y < ul.y                      {return Fail;}
+        if bx.x - ul.x != br.y - ul.y       {return Fail;}
+        if board[play.y][play.x] != Empty   {return Fail;}
+    }
+
+    fn validate_tile(self, tile: Tile) -> bool {
+        if tile.x >= self.board.len() {return false;}
+        if tile.y >= self.board.len() {return false;}
+        return true;
     }
 }
 
