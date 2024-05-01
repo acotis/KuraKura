a6ab3480920b33aba9b6cf375ad1d5372a3ce701
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-05-01 00:59:12 -0400

    Added basic game board struct.

---
+++ src/game.rs
@@ -0,0 +1,62 @@
+
+use std::fmt::Formatter;
+use std::fmt::Display;
+use std::fmt::Error;
+
+use crate::game::Color::*;
+
+#[derive(Clone, Copy)]
+enum Color {
+    Black,
+    White,
+    Empty,
+}
+
+enum GameOutcome {
+    BlackWin,
+    WhiteWin,
+    Draw,
+}
+
+enum PlayResult {
+    Fail,
+    Succeed(Option<GameOutcome>),
+}
+
+
+pub struct Twirl {
+    size:       usize,
+    board:      Vec<Vec<Color>>,
+    next_turn:  Color,
+    outcome:    Option<GameOutcome>, // None until the game is over
+}
+
+impl Twirl {
+    pub fn new(size: usize) -> Self {
+        Twirl {
+            size:       size,
+            board:      vec![vec![Empty; size]; size],
+            next_turn:  Black,
+            outcome:    None,
+
+        }
+    }
+}
+
+impl Display for Twirl {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        for row in &self.board {
+            for space in row {
+                match space {
+                    Black => {write!(f, "x")?;},
+                    White => {write!(f, "o")?;},
+                    Empty => {write!(f, ".")?;},
+                }
+            }
+            write!(f, "\n")?;
+        }
+        
+        Ok(())
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+
+mod game;
+use crate::game::Twirl;
+
 fn main() {
-    println!("Hello, world!");
+    let game = Twirl::new(9);
+    println!("{game}");
 }
