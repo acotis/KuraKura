ad5cae5e349bc4378667931c6eec982ff90fa0e4
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-05-19 02:16:14 -0400

    Moved some code around.

--- backend/src/game.rs
+++ backend/src/game.rs
@@ -407,39 +407,5 @@ impl Display for Twirl {
 
         Ok(())
     }
-
-}
-
-fn juxtapose(a: &str, b: &str) -> String {
-    let mut a_lines = a.lines();
-    let mut b_lines = b.lines();
-    let a_width = a.lines().fold(0, |best, next| best.max(next.len()));
-
-    let mut ret = "".to_string();
-    let margin = 4;
-    
-    loop {
-        match (a_lines.next(), b_lines.next()) {
-            (Some(a_line), Some(b_line)) => {
-                ret.push_str(a_line);
-                ret.push_str(&" ".repeat(a_width - a_line.len() + margin));
-                ret.push_str(b_line);
-            },
-            (None, Some(b_line)) => {
-                ret.push_str(&" ".repeat(a_width + margin));
-                ret.push_str(b_line);
-            },
-            (Some(a_line), None) => {
-                ret.push_str(a_line);
-            },
-            (None, None) => {
-                break;
-            },
-        }
-
-        ret.push_str("\n");
-    }
-
-    return ret;
 }
 
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,9 +10,9 @@ use crate::game::Player::*;
 fn main() {
     let mut game = Twirl::new(9, 2);
     game.play(Black, 0, 0);
-    game.spin(Black, 4, 4, 1, CW).unwrap();
+    game.spin(Black, 0, 0, 1, CW).unwrap();
     game.play(White, 0, 3);
-    game.spin(White, 4, 4, 1, CW);
+    game.spin(White, 0, 0, 1, CW);
     println!("{}", game);
 }
 
@@ -34,8 +34,40 @@ fn play(game: &mut Twirl, player: Player, r: usize, c: usize) -> String {
 
     let mut ret = format!("{player:?} plays at ({r}, {c})... => {result:?}\n\n");
 
-    //ret.push_str(&juxtapose(&initial, &ending));
+    ret.push_str(&juxtapose(&initial, &ending));
     return ret;
 }
 
+fn juxtapose(a: &str, b: &str) -> String {
+    let mut a_lines = a.lines();
+    let mut b_lines = b.lines();
+    let a_width = a.lines().fold(0, |best, next| best.max(next.len()));
+
+    let mut ret = "".to_string();
+    let margin = 4;
+    
+    loop {
+        match (a_lines.next(), b_lines.next()) {
+            (Some(a_line), Some(b_line)) => {
+                ret.push_str(a_line);
+                ret.push_str(&" ".repeat(a_width - a_line.len() + margin));
+                ret.push_str(b_line);
+            },
+            (None, Some(b_line)) => {
+                ret.push_str(&" ".repeat(a_width + margin));
+                ret.push_str(b_line);
+            },
+            (Some(a_line), None) => {
+                ret.push_str(a_line);
+            },
+            (None, None) => {
+                break;
+            },
+        }
+
+        ret.push_str("\n");
+    }
+
+    return ret;
+}
 
