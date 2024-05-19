755c30b811ba1129aee018fb8eb514dae289c066
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-05-19 02:27:08 -0400

    Added correct (visual) length detection for ANSI-formatted strings.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,12 +8,6 @@ use crate::game::SpinDirection::*;
 use crate::game::Player::*;
 
 fn main() {
-    let mut game = Twirl::new(9, 2);
-    game.play(Black, 0, 0);
-    game.spin(Black, 0, 0, 1, CW).unwrap();
-    game.play(White, 0, 3);
-    game.spin(White, 0, 0, 1, CW);
-    println!("{}", game);
 }
 
 #[test]
@@ -32,7 +26,7 @@ fn play(game: &mut Twirl, player: Player, r: usize, c: usize) -> String {
     let result  = game.play(player, r, c);
     let ending  = game.to_string();
 
-    let mut ret = format!("{player:?} plays at ({r}, {c})... => {result:?}\n\n");
+    let mut ret = format!("{player:?} plays at ({r}, {c})... => {result:?}\n");
 
     ret.push_str(&juxtapose(&initial, &ending));
     return ret;
@@ -41,16 +35,16 @@ fn play(game: &mut Twirl, player: Player, r: usize, c: usize) -> String {
 fn juxtapose(a: &str, b: &str) -> String {
     let mut a_lines = a.lines();
     let mut b_lines = b.lines();
-    let a_width = a.lines().fold(0, |best, next| best.max(next.len()));
+    let a_width = a.lines().fold(0, |best, next| best.max(len_colored(next)));
 
     let mut ret = "".to_string();
-    let margin = 4;
+    let margin = 2;
     
     loop {
         match (a_lines.next(), b_lines.next()) {
             (Some(a_line), Some(b_line)) => {
                 ret.push_str(a_line);
-                ret.push_str(&" ".repeat(a_width - a_line.len() + margin));
+                ret.push_str(&" ".repeat(a_width - len_colored(a_line) + margin));
                 ret.push_str(b_line);
             },
             (None, Some(b_line)) => {
@@ -69,5 +63,20 @@ fn juxtapose(a: &str, b: &str) -> String {
     }
 
     return ret;
+}
+
+// Visual length of an ANSI-colored string.
+
+fn len_colored(s: &str) -> usize {
+    let mut ret = 0;
+    let mut counting = true;
+
+    for c in s.to_owned().chars() {
+        if c == '\x1b' {counting = false;}
+        if counting {ret += 1;}
+        if c == 'm' {counting = true;}
+    }
+
+    ret
 }
 
