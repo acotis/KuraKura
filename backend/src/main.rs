69ec61f2dcee5f72aa2abaf63c40b499d97ff6a4
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-06-22 13:19:19 -0700

    Got it to send json.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,26 +6,30 @@ use axum::{
     routing::get,
     response::{IntoResponse, Response},
     Router,
+    Json,
 };
+
+use serde::Serialize;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Secret string for Lynn" }));
-    //let app = Router::new().route("/ws", get(handler));
-    //let app = Router::new().route("/ws", get(send_json));
+    //let app = Router::new().route("/", get(|| async { "Secret string for Lynn" }));
+    //let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/", get(send_json));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-//struct MyStruct {
-    //first_field: u32,
-    //second_field: String,
-//}
+#[derive(Serialize)]
+struct MyStruct {
+    first_field: u32,
+    second_field: String,
+}
 
-//async fn send_json() -> Json<MyStruct> {
-//
-//}
+async fn send_json() -> Json<MyStruct> {
+    Json(MyStruct { first_field: 1, second_field: "hello".into() })
+}
 
 
 async fn handler(ws: WebSocketUpgrade) -> Response {
