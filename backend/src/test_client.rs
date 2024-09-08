269cda9c9a67ddff393f0a3469db769fa3df776f
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-09-08 00:49:58 -0400

    Working on simulating clients for the test.

--- backend/src/test_client.rs
+++ backend/src/test_client.rs
@@ -2,8 +2,9 @@
 use futures_util::{SinkExt, StreamExt, stream::{SplitSink, SplitStream}};
 use http::Uri;
 use tokio_websockets::{ClientBuilder, Message, WebSocketStream, MaybeTlsStream};
-
 use tokio::net::TcpStream;
+
+use crate::server::UserResponse;
 
 static mut RESPONSES: Vec<String> = Vec::<String>::new();
 
@@ -53,8 +54,11 @@ async fn follow(client_id: usize, mut client: SplitStream<WebSocketStream<MaybeT
     }
 }
 
-fn get_response(client_id: usize) -> String {
-    unsafe {RESPONSES[client_id-1].clone()}
+fn get_response(client_id: usize) -> UserResponse {
+    let string = unsafe {RESPONSES[client_id-1].clone()};
+
+    serde_json::from_str(&string)
+                .expect("server's response was not JSON representing a valid value")
 }
 
 pub async fn run_test_client(id: usize) {
@@ -70,10 +74,65 @@ pub async fn run_test_client(id: usize) {
 
     pause(1000).await;
 
+    //let cu  = format!(r#""Register""#);
+
+    let cu = "hi";
+
     match id {
-        1 => send(id, &mut sender, "Hello world").await,
-        2 => send(id, &mut sender, "hi there").await,
+        1 => send(id, &mut sender, cu).await,
+        2 => send(id, &mut sender, cu).await,
+        2 => send(id, &mut sender, cu).await,
         _ => panic!(),
     }
+
+    //let response = get_response(id);
+
+
+
+    /*
+    let Ok(AccountRegistered {id: _evan}) = call(&mut server, &evan1, cu) else {panic!();};
+    let Ok(AccountRegistered {id: _lynn}) = call(&mut server, &lynn1, cu) else {panic!();};
+    let Ok(AccountRegistered {id: _lexi}) = call(&mut server, &lexi1, cu) else {panic!();};
+
+    let sn1 = &format!(r#"{{"SetName": {{"name": "Evan is my name"}}}}"#);
+    let sn2 = &format!(r#"{{"SetName": {{"name": "Laqme"}}}}"#);
+    let sn3 = &format!(r#"{{"SetName": {{"name": "The Lex"}}}}"#);
+
+    let Ok(Okay) = call(&mut server, &evan1, sn1) else {panic!();};
+    let Ok(Okay) = call(&mut server, &lynn1, sn2) else {panic!();};
+    let Ok(Okay) = call(&mut server, &lexi1, sn3) else {panic!();};
+
+    let cr = &format!(r#""CreateRoom""#);
+
+    let Ok(RoomCreated {id:  room1}) = call(&mut server, &lynn1, cr) else {panic!();};
+    let Ok(RoomCreated {id: _room2}) = call(&mut server, &lexi1, cr) else {panic!();};
+
+    let jr  = &format!(r#"{{"JoinRoom": {{"room": "{room1}"}}}}"#);
+
+    let Ok(Okay) = call(&mut server, &evan1, jr) else {panic!();};
+
+    let tt1 = &format!(r#"{{"TakeTurn": {{"turn": {{"player": "Black", "play_row": 0, "play_col": 0, "spin_ul_row": 0, "spin_ul_col": 0, "spin_size": 3, "spin_dir": "CW"}}}}}}"#);
+    let tt2 = &format!(r#"{{"TakeTurn": {{"turn": {{"player": "White", "play_row": 1, "play_col": 2, "spin_ul_row": 1, "spin_ul_col": 2, "spin_size": 1, "spin_dir": "CCW"}}}}}}"#);
+
+    let Ok(Okay) = call(&mut server, &lynn1, tt1) else {panic!();};
+    let Ok(Okay) = call(&mut server, &evan1, tt2) else {panic!();};
+
+    print!("{server}");
+
+    println!("{}", cu);
+    println!("{}", sn1);
+    println!("{}", sn2);
+    println!("{}", sn3);
+    println!("{}", cr1);
+    println!("{}", cr2);
+    println!("{}", jr);
+    println!("{}", tt1);
+    println!("{}", tt2);
+    */
+
+
+
+
+
 }
 
