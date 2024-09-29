002a968b52cddfd93f03bbae8e22f475acf4134c
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2024-09-29 16:00:30 -0400

    Working on refactoring package structure.

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,13 +1,4 @@
 
-mod types;
-mod game;
-mod cell;
 pub mod server;
-pub mod test_client; // todo remove this
-pub mod server_types; // todo leave this in only for the server library
-pub mod server_message_types; // todo leave this in only for the server library
-pub mod copy_id; // todo leave this in only for the server library
-
-pub use game::*;
-pub use types::*;
+pub mod game;
 
---
+++ backend/src/server/mod.rs
@@ -0,0 +1,7 @@
+
+pub mod copy_id;
+pub mod server_message_types;
+pub mod server;
+pub mod server_types;
+pub mod test_client;
+
