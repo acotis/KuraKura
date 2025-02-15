3ca81cb23c592a6b177131c255b3bd7c1eefb8e7
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2025-02-15 15:45:14 -0500

    Probably got most of the way out of type hell after changing around some type parameters.

--- backend/src/server/game.rs
+++ backend/src/server/game.rs
@@ -3,12 +3,12 @@ use std::fmt::{Debug, Display};
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 
-pub trait Game : Debug + Display + Clone {
+pub trait Game : Serialize + Display + Clone {
     type Parameters : DeserializeOwned;
+    type Turn : Serialize + DeserializeOwned + Clone;
     type Outcome;
     type PlayerRole : Serialize;
-    type Turn : Serialize + DeserializeOwned + Clone;
-    type TurnError : Clone + PartialEq + Eq + Serialize + Send;
+    type TurnError : Serialize;
 
     fn new(parameters: Self::Parameters) -> Self;
     fn add_player(&mut self) -> Self::PlayerRole;
