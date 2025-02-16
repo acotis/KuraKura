83d09ccd7b9747e440d05e7aaf830da4c8d59f88
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2025-02-15 23:56:17 -0500

    Updated todo.

--- backend/src/server/game.rs
+++ backend/src/server/game.rs
@@ -6,7 +6,7 @@ use serde::de::DeserializeOwned;
 pub trait Game : Serialize + Display + Clone {
     type Parameters : DeserializeOwned;
     type Turn : Serialize + DeserializeOwned + Clone;
-    type Outcome;
+    type Outcome: Serialize;
     type PlayerRole : Serialize + Clone;
     type TurnError : Serialize;
 
