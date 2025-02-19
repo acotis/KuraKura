5e901dfb8ad9e5a538a3b504681019701c8b0db8
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2025-02-18 23:54:19 -0500

    Added ideas.

--- backend/src/server/game.rs
+++ backend/src/server/game.rs
@@ -16,3 +16,29 @@ pub trait Game : Serialize + Display + Clone {
         Result<Option<Self::Outcome>, Self::TurnError>;
 }
 
+/* Hypothetical awesome version of this trait:
+ *
+ * pub trait Game {
+ *     type GameState,          // Depicts the state of the game.
+ *     type PlayerRole,         // What roles each player can have.
+ *     type Parameters,         // Used to create the game.
+ *     type ParametersError,    // Error when creating the game.
+ *     type Turn,               // Take a turn.
+ *     type TurnError,          // Error when taking your turn.
+ *
+ *     fn new(Parameters) -> Result<Self, ParametersError>;
+ *     fn turn(usize, Turn) -> Result<(), TurnError>;
+ *     fn add_player() -> PlayerRole;
+ *     fn current_state() -> GameState;
+ * }
+ *
+ * // Note: current_state() is a separate method because calls
+ * // add_player() can modify the game state in ways that can't
+ * // naturally be captured by the assigned PlayerRole. For
+ * // example, a player might spawn into a game world and be
+ * // given a starting location.
+ * //
+ * // You could argue that we should 
+ *
+ */
+
